259f194d90c79fdc688ea457729e066f682ed8dc
pylypchukroman <romanpylypchuk@gmail.com>
2023-11-16 10:31:45 +0200

    added solutions for generic

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,7 +3,7 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge (objA, objB) {
+function merge<A, B> (objA: A, objB: B): A & B {
   return Object.assign(objA, objB);
 }
 
